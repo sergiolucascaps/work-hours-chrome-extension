5a200463a58b2b6dba13b78ce98736bb2e31f2a5
Sergio Lucas <sergiolucascaps@hotmail.com>
2023-08-12 00:53:37 -0300

    extension popup modifications @sergiolucascaps

--- src/html/extension-body/extension-body.js
+++ src/html/extension-body/extension-body.js
@@ -1,15 +1,93 @@
+class WorkHoursConfiguration {
+
+    workload = 8;
+    constructor(workload) {
+        this.workload = workload;
+    }
+
+    setWorkload(workload) {
+        let expression = new RegExp("^([0-9])");
+
+        if(expression.test(workload)) alert("OK");
+        else alert("FALHOU");
+    }
+}
+
+let config = new WorkHoursConfiguration(8);
+
 async function analizarBatidas() {
     const [currentTab] = await getCurrentTab();
     console.log(currentTab);
-    await chrome.tabs.sendMessage(currentTab.id, {hello: "World", message: "Minha mensagem legal"});
+    await chrome.tabs.sendMessage(currentTab.id, config);
 }
 
 async function getCurrentTab() {
     return await chrome.tabs.query({active: true, currentWindow: true});
 }
 
+function createElementsToModifyParams() {
+    var section = document.getElementById("params");
+    section.innerHTML = '';
+    
+    const parametersDiv = document.createElement("div");
+    parametersDiv.id = "parameters";
+
+    const fieldset = document.createElement("fieldset");
+    let legend = document.createElement("legend");
+    legend.innerText = "Parâmetros";
+
+    let hoursDiv = document.createElement("div");
+    hoursDiv.id = "hours-div";
+
+    let hoursField = document.createElement("input");
+    hoursField.setAttribute("type", "text");
+    hoursField.placeholder = "carga horária (dia)";
+    hoursField.value = "8 horas";
+
+    let chkExactDiv = document.createElement("div");
+    chkExactDiv.id = "exact-div";
+
+    let exactField = document.createElement("input");
+    exactField.id = "exact-chk";
+    exactField.setAttribute("type", "checkbox");
+
+    let exactLabel = document.createElement("label");
+    exactLabel.setAttribute("for", "exact-chk");
+    exactLabel.textContent = "Horário exato?";
+
+    let btnSave = document.createElement("button");
+    btnSave.id = "btn-save";
+    btnSave.innerText = "Salvar";
+
+    chkExactDiv.appendChild(exactField);
+    chkExactDiv.appendChild(exactLabel);
+    hoursDiv.appendChild(hoursField);
+    fieldset.appendChild(legend);
+    fieldset.appendChild(hoursDiv);
+    fieldset.appendChild(chkExactDiv);
+    fieldset.appendChild(btnSave);
+    parametersDiv.appendChild(fieldset);
+    section.appendChild(parametersDiv);
+
+    addSaveListener();
+}
+
 // add listeners
+
+function addSaveListener() {
+    document.getElementById("btn-save").addEventListener("click", async () => {
+        alert("Is in TODO list...");
+        var section = document.getElementById("params");
+        section.innerHTML = '';
+        //TODO: @sergiolucascaps - Implementar.
+        // Pegar dados da caixa de texto e ckeckbox e salvar nas configurações.
+    });
+}
+
 document.getElementById("btn-analize").addEventListener("click", async () => {
-    const [currentTab] = await getCurrentTab();
-    await chrome.tabs.sendMessage(currentTab.id, {hello: "World", message: "Minha mensagem legal"});
+    await analizarBatidas()
+});
+
+document.getElementById("btn-modify-params").addEventListener("click", async () => {
+    await createElementsToModifyParams();
 });
